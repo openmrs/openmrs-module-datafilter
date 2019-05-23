ca329f2d68af60aae459c79ac48b9e3f015241f2
Wyclif Luyima <wyclif.luyima@gmail.com>
2019-05-22 23:32:41 -0400

    Applied auto format changes

--- api/src/main/java/org/openmrs/module/datafilter/DataFilterActivator.java
+++ api/src/main/java/org/openmrs/module/datafilter/DataFilterActivator.java
@@ -8,49 +8,49 @@ import org.apache.commons.logging.LogFactory;
 import org.openmrs.module.BaseModuleActivator;
 
 public class DataFilterActivator extends BaseModuleActivator {
-
-    protected Log log = LogFactory.getLog(getClass());
-
-    /**
-     * @see BaseModuleActivator#started()
-     */
-    @Override
-    public void started() {
-        if (log.isInfoEnabled()) {
-            log.info("Data Filter Module started");
-        }
-    }
-
-    /**
-     * @see BaseModuleActivator#stopped()
-     */
-    @Override
-    public void stopped() {
-        if (log.isInfoEnabled()) {
-            log.info("Data Filter Module stopped");
-        }
-    }
-
-    /**
-     * @see BaseModuleActivator#willStart()
-     */
-    @Override
-    public void willStart() {
-        if (log.isInfoEnabled()) {
-            log.info("Adding filter annotations");
-        }
-        //TODO Apply annotations
-    }
-
-    /**
-     * @see BaseModuleActivator#willStop()
-     */
-    @Override
-    public void willStop() {
-        if (log.isInfoEnabled()) {
-            log.info("Removing filter annotations");
-        }
-        //Remove Annotations
-    }
-
+	
+	protected Log log = LogFactory.getLog(getClass());
+	
+	/**
+	 * @see BaseModuleActivator#started()
+	 */
+	@Override
+	public void started() {
+		if (log.isInfoEnabled()) {
+			log.info("Data Filter Module started");
+		}
+	}
+	
+	/**
+	 * @see BaseModuleActivator#stopped()
+	 */
+	@Override
+	public void stopped() {
+		if (log.isInfoEnabled()) {
+			log.info("Data Filter Module stopped");
+		}
+	}
+	
+	/**
+	 * @see BaseModuleActivator#willStart()
+	 */
+	@Override
+	public void willStart() {
+		if (log.isInfoEnabled()) {
+			log.info("Adding filter annotations");
+		}
+		//TODO Apply annotations
+	}
+	
+	/**
+	 * @see BaseModuleActivator#willStop()
+	 */
+	@Override
+	public void willStop() {
+		if (log.isInfoEnabled()) {
+			log.info("Removing filter annotations");
+		}
+		//Remove Annotations
+	}
+	
 }

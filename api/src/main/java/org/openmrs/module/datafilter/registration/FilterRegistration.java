2a10030b4fb0f6a41b90e16d589f2b367880d993
Wyclif Luyima <wyclif@openmrs.org>
2019-10-02 22:41:40 -0500

    Moved FilterParameter and FilterRegistration to a new registration package
    Signed-off-by: Wyclif Luyima <wyclif@openmrs.org>

--- api/src/main/java/org/openmrs/module/datafilter/registration/FilterRegistration.java
+++ api/src/main/java/org/openmrs/module/datafilter/registration/FilterRegistration.java
@@ -7,7 +7,7 @@
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
-package org.openmrs.module.datafilter;
+package org.openmrs.module.datafilter.registration;
 
 import java.util.List;
 

3cd3af5a4a66c6171ab13dcb77ad90417b713c22
icrc-toliveira <70152448+icrc-toliveira@users.noreply.github.com>
2021-11-04 14:59:03 +0000

    FILTER-9: Add encounter type filter. (#14)
    * FILTER-9 - Add encounter type filter
    
    * FILTER-9: Updated Testes
    
    * FILTER-9 - Add encounter type filter
    
    * FILTER-9 - Add encounter type tests
    
    * FILTER-9 - Reverted unnecessary changes
    
    * FILTER-9 - Optimize imports
    
    * FILTER-9 - Add encounter type filter + tests

---
+++ api/src/test/java/org/openmrs/module/datafilter/impl/EncTypePrivilegeBasedFilterTest.java
@@ -0,0 +1,70 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.datafilter.impl;
+
+import org.hibernate.SessionFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.EncounterType;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.datafilter.TestConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class EncTypePrivilegeBasedFilterTest extends BaseEncTypeViewPrivilegeBasedFilterTest {
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Before
+	public void before() {
+		executeDataSet(TestConstants.ROOT_PACKAGE_DIR + "privilegedEncounters.xml");
+	}
+	
+	@Test
+	public void getEncounterType_shouldIncludeEncountersThatRequireAPrivilegeAndTheUserHasIt() {
+		reloginAs("dyorke", "test");
+		assertFalse(Context.getAuthenticatedUser().hasPrivilege(PRIV_MANAGE_CHEMO_PATIENTS));
+		
+		int expCount = 3;
+		List ecounterTypelist = sessionFactory.getCurrentSession().createCriteria(EncounterType.class).list();
+		assertEquals(expCount, ecounterTypelist.size());
+		
+		DataFilterTestUtils.addPrivilege(PRIV_MANAGE_CHEMO_PATIENTS);
+		expCount = 4;
+		ecounterTypelist = sessionFactory.getCurrentSession().createCriteria(EncounterType.class).list();
+		assertEquals(expCount, ecounterTypelist.size());
+	}
+
+	@Test
+	public void getCondition_shouldNotReturnEncounterTypesThatRequireAPrivilege() {
+		reloginAs("dyorke", "test");
+		assertFalse(Context.getAuthenticatedUser().hasPrivilege(PRIV_MANAGE_CHEMO_PATIENTS));
+		List encounterTypes = sessionFactory.getCurrentSession().createCriteria(EncounterType.class).list();
+		assertEquals(3, encounterTypes.size());
+	}
+	
+	@Test
+	public void getCondition_shouldReturnAllEncounterTypesIfTheAuthenticatedUserIsASuperUser() {
+		assertTrue(Context.getAuthenticatedUser().isSuperUser());
+		List encounterTypes = sessionFactory.getCurrentSession().createCriteria(EncounterType.class).list();
+		assertEquals(4, encounterTypes.size());
+	}
+	
+	@Test
+	public void getCondition_shouldReturnAllEncounterTypesIfEncTypeViewPrivFilteringIsDisabled() {
+		DataFilterTestUtils.disableEncTypeViewPrivilegeFiltering();
+		List encounterTypes = sessionFactory.getCurrentSession().createCriteria(EncounterType.class).list();
+		assertEquals(4, encounterTypes.size());
+	}
+}

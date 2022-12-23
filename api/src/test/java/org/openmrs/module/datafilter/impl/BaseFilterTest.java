b79b12a4c9cdcbfe698ba74e7150b75cfeb7581a
ruhanga <ruhanganathan@gmail.com>
2022-12-23 10:13:17 +0300

    FILTER-12: Producing failing tests

--- api/src/test/java/org/openmrs/module/datafilter/impl/BaseFilterTest.java
+++ api/src/test/java/org/openmrs/module/datafilter/impl/BaseFilterTest.java
@@ -9,30 +9,145 @@
  */
 package org.openmrs.module.datafilter.impl;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Properties;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.DatabaseUnitRuntimeException;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.ext.mysql.MySqlDataTypeFactory;
+import org.dbunit.operation.DatabaseOperation;
 import org.hibernate.cfg.Environment;
+import org.hibernate.dialect.MySQL5Dialect;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.openmrs.GlobalProperty;
 import org.openmrs.api.AdministrationService;
 import org.openmrs.api.context.Context;
 import org.openmrs.api.context.UsernamePasswordCredentials;
+import org.openmrs.liquibase.ChangeLogVersionFinder;
+import org.openmrs.liquibase.ChangeSetExecutorCallback;
 import org.openmrs.module.datafilter.TestConstants;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
+import org.openmrs.test.TestUtil;
+import org.openmrs.util.DatabaseUpdater;
 import org.openmrs.util.PrivilegeConstants;
+import org.testcontainers.containers.MySQLContainer;
+import liquibase.changelog.ChangeSet;
 
 public abstract class BaseFilterTest extends BaseModuleContextSensitiveTest {
 	
+	private static final Logger log = LoggerFactory.getLogger(BaseFilterTest.class);
+	
+	private static MySQLContainer<?> mysqlContainer = new MySQLContainer("mysql:5.7.31");
+	
+	private static String databaseUrl = "jdbc:mysql://localhost:DATABASE_PORT/openmrs?autoReconnect=true&sessionVariables=default_storage_engine%3DInnoDB&useUnicode=true&characterEncoding=UTF-8";
+	
+	private static String databaseUsername = "user";
+	
+	private static String databaseUserPasswword = "12345678";
+	
+	private static String databaseDialect = MySQL5Dialect.class.getName();
+	
+	private static String databaseDriver = "com.mysql.jdbc.Driver";
+	
+	@BeforeClass
+	public static void setupMySqlDb() throws IOException {
+		
+		mysqlContainer.withDatabaseName("openmrs");
+		mysqlContainer.withUsername(databaseUsername);
+		mysqlContainer.withPassword(databaseUserPasswword);
+		mysqlContainer.start();
+		
+		System.setProperty("databaseUrl",
+		    databaseUrl.replaceAll("DATABASE_PORT", String.valueOf(mysqlContainer.getMappedPort(3306))));
+		System.setProperty("databaseUsername", databaseUsername);
+		System.setProperty("databasePassword", databaseUserPasswword);
+		System.setProperty("databaseDriver", databaseDriver);
+		System.setProperty("useInMemoryDatabase", "false");
+	}
+	
+	@Override
+	public void baseSetupWithStandardDataAndAuthentication() throws SQLException {
+		// Open a session if needed
+		if (!Context.isSessionOpen()) {
+			Context.openSession();
+		}
+		
+		ChangeLogVersionFinder changeLogVersionFinder = new ChangeLogVersionFinder();
+		
+		String liquibaseSchemaFileName = changeLogVersionFinder.getLatestSchemaSnapshotFilename().get();
+		//		String liquibaseCoreDataFileName = changeLogVersionFinder.getLatestCoreDataSnapshotFilename().get();
+		
+		try {
+			DatabaseUpdater.executeChangelog(liquibaseSchemaFileName,
+			    new PrintingChangeSetExecutorCallback("OpenMRS schema file"));
+			String datafilterLiquibase = getClass().getClassLoader().getResource("liquibase.xml").toURI().getPath();
+			DatabaseUpdater.executeChangelog("liquibase.xml",
+			    new PrintingChangeSetExecutorCallback("Datafilter schema file"));
+		}
+		catch (Exception ex) {
+			throw new RuntimeException("Could not update database.", ex);
+		}
+		executeDataSet(INITIAL_XML_DATASET_PACKAGE_PATH);
+		
+		//Commit so that it is not rolled back after a test.
+		getConnection().commit();
+		
+		authenticate();
+		Context.clearSession();
+	}
+	
 	@Override
 	public Properties getRuntimeProperties() {
-		Properties props = super.getRuntimeProperties();
-		//Fixes the error reported by the h2 driver in tests
-		props.setProperty(Environment.URL, props.getProperty(Environment.URL) + ";DB_CLOSE_ON_EXIT=FALSE");
-		return props;
+		if (runtimeProperties == null) {
+			runtimeProperties = TestUtil.getRuntimeProperties(getWebappName());
+		}
+		runtimeProperties.setProperty(Environment.URL,
+		    databaseUrl.replaceAll("DATABASE_PORT", String.valueOf(mysqlContainer.getMappedPort(3306))));
+		
+		runtimeProperties.setProperty(Environment.USER, "user");
+		runtimeProperties.setProperty(Environment.PASS, "12345678");
+		runtimeProperties.setProperty("connection.username", "user");
+		runtimeProperties.setProperty("connection.password", "12345678");
+		runtimeProperties.setProperty("connection.url",
+		    databaseUrl.replaceAll("DATABASE_PORT", String.valueOf(mysqlContainer.getMappedPort(3306))));
+		runtimeProperties.setProperty(Environment.HBM2DDL_AUTO, "none");
+		runtimeProperties.setProperty(Environment.DIALECT, databaseDialect);
+		
+		return runtimeProperties;
+	}
+	
+	@Override
+	public void executeDataSet(String dataset) {
+		try {
+			turnOffDBConstraints(getConnection());
+			super.executeDataSet(dataset);
+			turnOnDBConstraints(getConnection());
+		}
+		catch (SQLException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+	
+	@Override
+	public IDatabaseConnection setupDatabaseConnection(Connection connection) throws DatabaseUnitException {
+		IDatabaseConnection dbUnitConn = new DatabaseConnection(connection);
+		
+		DatabaseConfig config = dbUnitConn.getConfig();
+		config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new MySqlDataTypeFactory());
+		
+		return dbUnitConn;
 	}
 	
 	@Before
-	public void beforeTestMethod() {
+	public void beforeTestMethod() throws SQLException {
 		executeDataSet(TestConstants.MODULE_TEST_DATASET_XML);
 	}
 	
@@ -68,6 +183,26 @@ public abstract class BaseFilterTest extends BaseModuleContextSensitiveTest {
 			as.saveGlobalProperty(gp);
 			Context.removeProxyPrivilege(PrivilegeConstants.MANAGE_GLOBAL_PROPERTIES);
 			Context.flushSession();
+		}
+	}
+	
+	private static class PrintingChangeSetExecutorCallback implements ChangeSetExecutorCallback {
+		
+		private int i = 1;
+		
+		private String message;
+		
+		public PrintingChangeSetExecutorCallback(String message) {
+			this.message = message;
+		}
+		
+		/**
+		 * @see ChangeSetExecutorCallback#executing(liquibase.changelog.ChangeSet, int)
+		 */
+		@Override
+		public void executing(ChangeSet changeSet, int numChangeSetsToRun) {
+			log.info(message + " (" + i++ + "/" + numChangeSetsToRun + "): Author: " + changeSet.getAuthor() + " Comments: "
+			        + changeSet.getComments() + " Description: " + changeSet.getDescription());
 		}
 	}
 	
